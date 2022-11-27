62a3936a18f8daf0edb3679582d5197c5c018464
anshal1 <101871035+anshal1@users.noreply.github.com>
2022-11-27 09:08:48 +0530

    adding error handling

--- src/pages/Cardpage/CardPage.jsx
+++ src/pages/Cardpage/CardPage.jsx
@@ -7,6 +7,7 @@ import Loading from "../Loading/Loading";
 const CardPage = () => {
   const [country, setcountry] = useState([]);
   const [loading, setloading] = useState(true);
+  const [error, seterror] = useState(null);
   const GetCountry = async () => {
     let url = "https://restcountries.com/v3.1/all";
     let data = await fetch(url);
@@ -31,16 +32,24 @@ const CardPage = () => {
     setcountry(res);
   };
   const searchbyname = async (country) => {
-    setloading(true);
-    let url = `https://restcountries.com/v3.1/name/${country}`;
-    let data = await fetch(url);
-    let res = await data.json();
-    if (res) {
-      setloading(false);
-    } else {
-      setloading(false);
+    try {
+      setloading(true);
+      let url = `https://restcountries.com/v3.1/name/${country}`;
+      let data = await fetch(url);
+      let res = await data.json();
+      if (res.status !== 404) {
+        setloading(false);
+        setcountry(res);
+      } else if(res.status === 404) {
+        setloading(false);
+        seterror(error);
+      }
+    } catch (error) {
+      if (error) {
+        setloading(false);
+        setcountry([]);
+      }
     }
-    setcountry(res);
   };
   useEffect(() => {
     GetCountry();
@@ -64,19 +73,25 @@ const CardPage = () => {
           <Loading />
         ) : (
           <div className="card_holder">
-            {country?.map((c) => {
-              return (
-                <Card
-                  img={c.flags.png}
-                  key={c.name.official}
-                  name={c.name.official}
-                  population={c.population}
-                  capital={c.capital ? c.capital : "None"}
-                  region={c.region}
-                  commonname={c.name.official}
-                />
-              );
-            })}
+            {error ? (
+              <h1>No Country Found</h1>
+            ) : (
+              <div className="card_holder">
+                {country?.map((c) => {
+                  return (
+                    <Card
+                      img={c.flags.png}
+                      key={c.name.official}
+                      name={c.name.official}
+                      population={c.population}
+                      capital={c.capital ? c.capital : "None"}
+                      region={c.region}
+                      commonname={c.name.official}
+                    />
+                  );
+                })}
+              </div>
+            )}
           </div>
         )}
       </div>
